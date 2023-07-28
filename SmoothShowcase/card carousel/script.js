73ddaee88b04fafeb67e08b5caacb4031e601859
Mischa <mfgoes1@gmail.com>
2023-07-29 00:22:37 +0200

    created new example using swiper.js

--- SmoothShowcase/card carousel/script.js
+++ SmoothShowcase/card carousel/script.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
     $('.carousel').slick({
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
         dots: true,
         centerMode: true,
         responsive: [
+            {
+                breakpoint: 1424,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
             {
                 breakpoint: 1024,
                 settings: {
